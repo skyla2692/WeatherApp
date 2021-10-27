2bb69d032a0b2d343d9e4b9c256a5933873398fd
skylalee <reloxo99@gmail.com>
2021-10-27 17:45:28 +0900

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import React, { useEffect, useState } from 'react';
+import { Dimensions, StyleSheet, Text, View, ScrollView } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [location, setLocation] = useState();
+  const [ok, setOk]= useState(true);
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+    }
+
+    const location = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    console.log(location);
+  };
+
+  useEffect(() => {
+    ask();
+    }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+
+      <ScrollView 
+        pagingEnabled   /* makes the pages to the scrolls (not allowing pages to be freely scrolled) */
+        horizontal    /* makes the pages to go horizontal instead of vertical */
+        showsHorizontalScrollIndicator={true}   /* if false -> makes the scrolling bar in the bottom disappear */
+        indicatorStyle="white"    /* change the color of your scoll bar + only works in IOS */
+        contentContainerStyle={styles.weather}  /* way to apply style under ScrollView Tag */
+        >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -14,8 +66,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightcoral",
   },
-});
+
+  city: {
+    flex: 0.8,
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 48,
+    fontWeight: "500",
+  },
+
+  weather: {
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 178,
+    marginTop: 70,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  }
+})
