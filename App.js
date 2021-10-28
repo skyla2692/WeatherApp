0f8b9f708e9937487cb862516d98009040cc5fef
skylalee <reloxo99@gmail.com>
2021-10-28 23:22:34 +0900

    everything from lecture & few adjustments

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import * as Location from 'expo-location';
 import React, { useEffect, useState } from 'react';
 import { Dimensions, StyleSheet, Text, View, ScrollView, ActivityIndicator } from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const API_KEY = "c01eb371fe0059414ff56f2768963b7c";
 
+const icons = {
+  "Clouds" : "cloudy",
+  "Rain" : "rains",
+  "Clear" : "day-sunny",
+  "Drizzle" : "rain",
+  "Thunderstorm" : "lightnings",
+  "Snow" : "snowflake",
+  "Atmosphere" : "cloudy-gusts",
+};
+
 export default function App() {
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
-  const [temp, setTemp] = useState([]);
   const [ok, setOk]= useState(true);
 
   const getWeather = async () => {
@@ -16,7 +26,7 @@ export default function App() {
 
     if(!granted){
       setOk(false);
-    }
+    };
 
     const { coords : { latitude, longitude }} = await Location.getCurrentPositionAsync({ accuracy: 5 });
     const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
@@ -24,8 +34,8 @@ export default function App() {
     setCity(location[0].city);
     const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
     const json = await response.json();
+
     setDays(json.daily);
-    setTemp(json.current.temp);
   };
 
   const date = new Date();
@@ -57,9 +67,12 @@ export default function App() {
             days.map((day, index) => 
               <View key={index} style={styles.day}>
                 <Text style={styles.date}>{date.getMonth()+1} / {date.getDate()}</Text>
-                <View style={styles.tempBox}>
-                  <Text style={styles.temp}>{parseFloat(temp).toFixed(1)}</Text>
-                  <Text style={styles.symbol}>°C</Text>
+                <View style={styles.iconBox}>
+                  <View style={styles.tempBox}>
+                    <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                    <Text style={styles.symbol}>°C</Text>
+                  </View>
+                  <Fontisto name={icons[day.weather[0].main]} size={68} color="black" style={{marginRight: 20}}/>
                 </View>
                 <Text style={styles.description}>{day.weather[0].main}</Text>
                 <Text style={styles.textDescription}>{day.weather[0].description}</Text>
@@ -78,15 +91,15 @@ const styles = StyleSheet.create({
   },
 
   city: {
-    flex: 1,
-    marginTop: 50,
+    flex: 1.5,
+    marginTop: 70,
     justifyContent: "center",
     alignItems: "center",
     //backgroundColor: 'blue',
   },
   cityName: {
-    fontSize: 48,
-    fontWeight: "500",
+    fontSize: 50,
+    fontWeight: "700",
   },
 
   weather: {
@@ -95,43 +108,57 @@ const styles = StyleSheet.create({
     //backgroundColor: 'red',
   },
 
-  date: {
-    fontSize: 30,
-    color: "black",
-    marginVertical: 20,
-    //backgroundColor: 'white',
-  },
-
   day: {
     width: SCREEN_WIDTH,
     alignItems: "flex-start",
-    marginLeft: 10,
+    paddingHorizontal: 15,
+    marginTop: -40,
+    marginBottom: 100,
     //backgroundColor: 'yellow',
   },
 
+  date: {
+    fontSize: 30,
+    fontWeight: "500",
+    marginTop: 5,
+    //backgroundColor: 'white',
+  },
+
+  iconBox: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    marginTop: -10,
+    //backgroundColor: "lightgreen"
+  },
   tempBox: {
     flexDirection: "row",
     alignItems: "center",
-    paddingTop: 20,
-    marginVertical: 20,
+    marginVertical: 50,
     //backgroundColor: 'teal',
   },
   temp: {
     fontSize: 90,
+    fontWeight: "600",
   },
   symbol: {
     fontSize: 75,
     paddingLeft: 10,
+    fontWeight: "500",
   },
 
   description: {
-    //backgroundColor: 'tomato',
     fontSize: 30,
-    marginVertical: 20,
+    fontWeight: "600",
+    marginTop: -10,
+    marginBottom: 10,
+    //backgroundColor: 'tomato',
   },
   textDescription: {
-    //backgroundColor: 'orange',
     fontSize: 20,
-    marginVertical: 20,
+    fontWeight: "500",
+    marginVertical: 10,
+    //backgroundColor: 'orange',
   }
 })
