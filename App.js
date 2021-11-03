3809a50cd01e692af95a245ebc1fb1b8c0759abc
skylalee <reloxo99@gmail.com>
2021-11-03 22:06:52 +0900

    UI changed

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import React, { useEffect, useState } from 'react';
 import { Dimensions, StyleSheet, Text, View, ScrollView, ActivityIndicator } from 'react-native';
 import { Fontisto } from '@expo/vector-icons';
 
+const config = require('./config/key');
+const API_KEY = config.API_KEY;
+
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
-const API_KEY = "c01eb371fe0059414ff56f2768963b7c";
 
 const icons = {
   "Clouds" : "cloudy",
@@ -14,6 +16,15 @@ const icons = {
   "Thunderstorm" : "lightnings",
   "Snow" : "snowflake",
   "Atmosphere" : "cloudy-gusts",
+  "Mist" : "cloudy-gusts",
+  "Smoke" : "fog",
+  "Haze" : "cloudy-gusts",
+  "Dust": "cloudy-gusts",
+  "Fog" : "fog",
+  "Sand" : "cloudy-gusts",
+  "Ash" : "cloudy-gusts",
+  "Squall" : "cloudy-gusts",
+  "Tornado" : "cloudy-gusts",
 };
 
 export default function App() {
@@ -59,27 +70,33 @@ export default function App() {
         /* indicatorStyle="white"    change the color of your scoll bar + only works in IOS */
         contentContainerStyle={styles.weather}  /* way to apply style under ScrollView Tag */
       >
-        <View style={styles.currentStatus}>
-          <View style={styles.currentIcon}>
-            <Text style={styles.currentTemp}>{parseFloat(currents.temp).toFixed(1)}°C</Text>
-            <View style={styles.currentDescBox}>
-              <Fontisto name={icons[currents.weather[0].main]} size={70} color="black" style={{marginTop: 20}}/> 
-              <Text style={styles.currentDescription}>{currents.weather[0].main}</Text>
+        <View style={styles.today}>
+          <View style={styles.currentStatus}>
+            <View style={styles.currentIcon}>
+              <Text style={styles.currentTemp}>{parseFloat(currents.temp).toFixed(1)}°C</Text>
+              <View style={styles.currentDescBox}>
+                <Fontisto name={icons[currents.weather[0].main]} size={56} color="black" style={{marginTop: 20}}/> 
+                <Text style={styles.currentDescription}>{currents.weather[0].main}</Text>
+              </View>
+            </View>
+            <View style={styles.currMaxMin}>
+              <Text style={styles.currMM}>{parseFloat(days[0].temp.max).toFixed(0)}°C</Text>
+              <Text style={styles.currMM}>/</Text>
+              <Text style={styles.currMM}>{parseFloat(days[0].temp.min).toFixed(0)}°C</Text>
             </View>
           </View>
-          <View style={styles.currMaxMin}>
-            <Text style={styles.currMM}>{parseFloat(days[0].temp.max).toFixed(1)}°C</Text>
-            <Text style={styles.currMM}>/</Text>
-            <Text style={styles.currMM}>{parseFloat(days[0].temp.min).toFixed(1)}°C</Text>
-          </View>
-
           <ScrollView
+            horizontal
+            scrollEnabled
             indicatorStyle="white"
             contentContainerStyle={styles.hourly}>
-            <View style={styles.everyHour}>
-              {/* <Fontisto name={icons[hours.weather[0].main]} size={30} color="black" style={{marginTop: 10}}/>
-              <Text style={styles.currentDescription}>{hours.weather[0].main}</Text> */} 
-            </View>
+            {hours.map((hour, index) =>
+              <View style={styles.everyHour}>
+                <Text style={styles.hourlyhour}>{new Date(hour.dt*1000).getHours()}시</Text>
+                <Fontisto name={icons[hour.weather[0].main]} size={30} color="black" style={{marginTop: 10}}/>
+                <Text style={styles.hourlyTemp}>{parseFloat(hour.temp).toFixed(1)}</Text>
+              </View>
+            )}
           </ScrollView>
         </View>
 
@@ -94,8 +111,8 @@ export default function App() {
             <View style={styles.weekBox}>
               <Text style={styles.date}>{new Date(day.dt*1000).toString().substring(4, 10)}</Text>
               <Fontisto name={icons[day.weather[0].main]} size={25} color="black"/>
-              <Text style={styles.temp}>{parseFloat(day.temp.max).toFixed(1)}°C</Text>
-              <Text style={styles.temp}>{parseFloat(day.temp.min).toFixed(1)}°C</Text>
+              <Text style={styles.temp}>{parseFloat(day.temp.max).toFixed(0)}°C</Text>
+              <Text style={styles.temp}>{parseFloat(day.temp.min).toFixed(0)}°C</Text>
             </View>
             )
           )}
@@ -107,23 +124,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "lightcoral",
+    backgroundColor: "#afdeee",
   },
 
   city: {
-    flex: 0.5,
+    flex: 1.2,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 30,
+    marginTop: 50,
     //backgroundColor: 'blue',
   },
   cityName: {
-    fontSize: 30,
+    fontSize: 32,
     fontWeight: "600",
   },
 
+  today: {
+    flex: 4,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginVertical: 10,
+    //backgroundColor: 'lime',
+  },
   currentStatus: {
-    height: "60%",
     marginVertical: 5,
     //backgroundColor: 'gold',
   },
@@ -136,23 +159,25 @@ const styles = StyleSheet.create({
   },
   currentTemp: {
     fontSize: 80,
-    marginTop: -10,
     fontWeight: "600",
   },
   currentDescBox: {
+    marginTop: 10,
     alignItems: 'center',
   },
   currentDescription: {
     fontSize: 20,
     fontWeight: "500",
+    marginTop: 10,
   },
   currMaxMin: {
     flexDirection: "row",
-    width: "80%",
+    width: "70%",
+    marginTop: -20,
+    marginBottom: 40,
     paddingHorizontal: 40,
     alignItems: "center",
     justifyContent: 'space-around',
-    marginTop: -20,
     //backgroundColor: "darkviolet",
   },
   currMM: {
@@ -162,8 +187,31 @@ const styles = StyleSheet.create({
 
   hourly: {
     flexDirection: "row",
+    height: 120,
     alignItems: "center",
-    backgroundColor: "green",
+    marginVertical: 10,
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    //backgroundColor: "green",
+  },
+  everyHour: {
+    flexDirection: "column",
+    width: 80,
+    alignItems: "center",
+    marginVertical: 20,
+    paddingHorizontal: 5,
+    //backgroundColor: 'fuchsia',
+  },
+  hourlyhour: {
+    fontSize: 20,
+    fontWeight: '500',
+    //backgroundColor: 'cornsilk'
+  },
+  hourlyTemp: {
+    fontSize: 20,
+    fontWeight: '500',
+    marginTop: 10,
+    //backgroundColor: 'coral',
   },
 
   weather: {
@@ -189,7 +237,7 @@ const styles = StyleSheet.create({
     marginVertical: 5,
     paddingHorizontal: 10,
     borderBottomWidth: 1,
-    backgroundColor: "#d67272",
+    backgroundColor: "#96d4e9"
   },
   date: {
     fontSize: 24,
