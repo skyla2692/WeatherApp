e2e36dd63c016c8e002757376848f150a68a78ac
skylalee <reloxo99@gmail.com>
2021-11-07 16:45:26 +0900

    function control -ing

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Dimensions, StyleSheet, Text, View, ScrollView, ActivityIndicator } from 'react-native';
 import { Fontisto } from '@expo/vector-icons';
 
-const config = require('./config/key');
-const API_KEY = config.API_KEY;
+const API_KEY = "c01eb371fe0059414ff56f2768963b7c";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
@@ -94,7 +93,7 @@ export default function App() {
             indicatorStyle="white"
             contentContainerStyle={styles.hourly}>
             {hours.map((hour) =>
-              <View style={styles.everyHour}>
+              <View key={hour.index} style={styles.everyHour}>
                 <Text style={styles.hourlyhour}>{new Date(hour.dt*1000).getHours()}시</Text>
                 <Fontisto name={icons[hour.weather[0].main]} size={30} color="black" style={{marginTop: 10}}/>
                 <Text style={styles.hourlyTemp}>{parseFloat(hour.temp).toFixed(1)}</Text>
@@ -113,7 +112,7 @@ export default function App() {
           </View>
           ) : (
           days.map((day) => 
-            <View style={styles.weekBox}>
+            <View key={day.index} style={styles.weekBox}>
               <Text style={styles.date}>{new Date(day.dt*1000).toString().substring(4, 10)}</Text>
               <Fontisto name={icons[day.weather[0].main]} size={25} color="black"/>
               <Text style={styles.temp}>{parseFloat(day.temp.max).toFixed(0)}°C</Text>
--- Log&Task/Tasks.md
+++ Log&Task/Tasks.md
@@ -1,23 +1,26 @@
 ## Issues to be handled..
 
-#### 2021. 10. 28.
+### 2021. 10. 28.
 - Find a way to show weekly forcast with date.
 - 'Day' should be next to 'Date' components.
 - Consider which temp to use for weekly forcast. (daily average / max & min / night & day / ect.)
 
-#### 2021. 11. 01.
+### 2021. 11. 01.
 - Re-design the UI of the weather page.
 - Init README.md
 
-#### 2021. 11. 02.
+### 2021. 11. 02.
 - Show Max and Min temp of the day under the 'currentTemp' component.
 - Get hourly data and make a scroll view in the middle of the screen.
 - Make github file public.
 
-#### 2021. 11. 03.
+### 2021. 11. 03.
 - Find out if modification on array is allowed in between jsx.
 
-#### 2021. 11. 05.
+### 2021. 11. 05.
 - Why is data always needed to be indented first to be compiled without error?
     - Is this API prob? or my JSX prob? WHYYYYY
 - Can background be changed with image without impacting any of UI?
+
+### 2021. 11. 07.
+- 
